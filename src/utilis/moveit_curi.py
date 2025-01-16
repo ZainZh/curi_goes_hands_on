15f8b71278e140e3af6f30c7637d0f7d38894853
Zain <1155169521@link.cuhk.edu.hk>
2025-01-16 20:44:07 +0800

    update a simple interface

--- src/utilis/moveit_curi.py
+++ src/utilis/moveit_curi.py
@@ -18,45 +18,47 @@ import geometry_msgs.msg
 from math import pi, tau, dist, fabs, cos
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
+from common import load_omega_config
+
 
 class MoveItCURI(object):
     def __init__(self, config="moveit_curi"):
         super(MoveItCURI, self).__init__()
+        self.config = load_omega_config(config)
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node('moveit_curi', anonymous=True)
+        rospy.init_node("moveit_curi", anonymous=True)
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
-        self.group_name = "panda_arm"
-        self.group = moveit_commander.MoveGroupCommander(self.group_name)
-        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                            moveit_msgs.msg.DisplayTrajectory,
-                                                            queue_size=20)
-        self.planning_frame = self.group.get_planning_frame()
-        self.eef_link = self.group.get_end_effector_link()
+
+        self.left_group_name = "left_arm"
+        self.left_group = moveit_commander.MoveGroupCommander(self.left_group_name)
+        self.left_eef_link = self.left_group.get_end_effector_link()
         self.group_names = self.robot.get_group_names()
-        self.box_name = ''
-        self.box_pose = geometry_msgs.msg.PoseStamped()
-        self.box_pose.header.frame_id = "panda_link0"
-        self.box_pose.pose.position.x = 0.4
-        self.box_pose.pose.position.y = 0.0
-        self.box_pose.pose.position.z = 0.5
-        self.box_name = "box"
-        self.scene.add_box(self.box_name, self.box_pose, size=(0.1, 0.1, 0.1))
-        self.group.set_max_velocity_scaling_factor(0.1)
-        self.group.set_max_acceleration_scaling_factor(0.1)
-        self.group.set_goal_position_tolerance(0.01)
-        self.group.set_goal_orientation_tolerance(0.01)
-        self.group.set_planning_time(5)
-        self.group.set_num_planning_attempts(5)
-        self.group.set_planner_id("RRTConnectkConfigDefault")
-        self.group.set_planning_time(5)
-        self.group.set_num_planning_attempts(5)
-        self.group.set_planner_id("RRTConnectkConfigDefault")
-        self.group.set_planning_time(5)
-        self.group.set_num_planning_attempts(5)
-        self.group.set_planner_id("RRTConnectkConfigDefault")
-        self.group.set_planning_time(5)
-        self.group.set_num_planning_attempts(5)
-        self.group.set_planner_id("RRTConnectkConfigDefault")
-        self.group.set_planning_time(5)
-        self.group.set_num_planning_attempts(5)
+
+        self.right_group_name = "right_arm"
+        self.right_group = moveit_commander.MoveGroupCommander(self.right_group_name)
+        self.right_eef_link = self.right_group.get_end_effector_link()
+
+    @property
+    def left_arm_joint_state(self):
+        return self.left_group.get_current_joint_values()
+
+    @property
+    def right_arm_joint_state(self):
+        return self.right_group.get_current_joint_values()
+
+    @property
+    def left_arm_pose(self):
+        return pose_to_list(self.left_group.get_current_pose().pose)
+
+    @property
+    def right_arm_pose(self):
+        return pose_to_list(self.right_group.get_current_pose().pose)
+
+
+if __name__ == "__main__":
+    moveit_curi = MoveItCURI()
+    print("left_arm_joint_state: ", moveit_curi.left_arm_joint_state)
+    print("right_arm_joint_state: ", moveit_curi.right_arm_joint_state)
+    print("left_arm_pose: ", moveit_curi.left_arm_pose)
+    print("right_arm_pose: ", moveit_curi.right_arm_pose)
